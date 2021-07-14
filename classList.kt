773c1ff4bc378a0b6952847cd4da0f4acde95e37
macbook <hanafiabdullah97@gmail.com>
2021-07-14 19:46:14 +0700

    section 14

--- classList.kt
+++ classList.kt
@@ -2,18 +2,59 @@ data class Student(val name : String, val age: Int, val semester : Int, val univ
 
 }
 
-data class footBallPlayer(val name: String, val age : Int, val club: String, val jersey_num: Int){
+data class footBallPlayer(val name: String, var age : Int, val club: String, val jersey_num: Int){
+
+    companion object{
+        fun seeAge(cName: String, cAge: Int, cClub: String, cJersey_num: Int): footBallPlayer{
+            var cAge = cAge
+            if (cAge > 30){
+                cAge = 40 - cAge
+            }
+            return footBallPlayer(cName, cAge, cClub, cJersey_num)
+        }
+
+    }
 
 }
 
 data class projectData(val name : String, var duration : Double, val User: String, val deadline: Int){
-    var count_duration = duration/30
+    companion object{
+        fun durationLimit(cName: String, cDuration: Double, cUser : String, cDeadline : Int): projectData{
+            cDuration/10000
+            return projectData(cName, cDuration, cUser, cDeadline)
+        }
+    }
 
 }
 
-data class peopleNearby(var name: String, var length: Int, var gadget: String){
-
+data class peopleNearby(var name: String, var length: Int, var gadget: String, var nearbyStatus: String){
+    companion object{
+        fun nearbyStatus(cName: String, cLength: Int, cGadget: String, cNearbyStatus : String ): peopleNearby{
+            var cLength = cLength
+            var cNearbyStatus = cNearbyStatus
+            if (cLength < 30000){
+                cNearbyStatus = "DEKAT"
+            }else{
+                cNearbyStatus = "JAUH"
+            }
+            return peopleNearby(cName, cLength, cGadget, cNearbyStatus)
+        }
+    }
 }
+
+
+
+data class StudentClass (var name : String, var age: Int, val semester : Int){
+
+    companion object {
+        fun editSemester(cName: String,cAge : Int, cSemester: Int): StudentClass{
+            var cSemester = cSemester/2
+            return StudentClass (cName, cAge, cSemester)
+        }
+    }
+}
+
+
 
 
 data class StudentWithID(val ID : Int,var name : String, var age: Int, val semester : Int) {
@@ -29,7 +70,7 @@ data class StudentWithID(val ID : Int,var name : String, var age: Int, val semes
 
         fun changeStudentName(cName : String, cAge: Int, cSemester : Int): StudentWithID{
 
-            var cName: String =  "${cName} + telyu"
+            var cName: String =  "${cName} + telkom"
             return StudentWithID(idData, cName, cAge, cSemester)
         }
     }
